6da197dd9ad8fbfd71c50af752b377299873dee4
Mustafa Dagher <dagher.mustafa@gmail.com>
2017-06-25 00:23:07 +0200

    minimize file access by using file object less

--- news-crowler/src/main/java/ai/mobiquity/sourcer/util/CsvUtils.java
+++ news-crowler/src/main/java/ai/mobiquity/sourcer/util/CsvUtils.java
@@ -16,25 +16,26 @@ public class CsvUtils {
         try {
             FileWriter fr = new FileWriter(filename + ".csv");
 
-            StringBuilder header = new StringBuilder();
-            fr.append("Title").append(CSV_SEPARATOR).append("Content").append(CSV_SEPARATOR).append("URL").append(CSV_SEPARATOR).append("Date").append(System.lineSeparator());
+            StringBuilder builder = new StringBuilder();
+            builder.append("Title").append(CSV_SEPARATOR).append("Content").append(CSV_SEPARATOR).append("URL").append(CSV_SEPARATOR).append("Date").append(System.lineSeparator());
 
             for (News news : newsList) {
                 String title = news.getTitle() != null ? news.getTitle().replace("\"", "") : "";
-                fr.append("\"" + title + "\"");
-                fr.append(CSV_SEPARATOR);
+                builder.append("\"" + title + "\"");
+                builder.append(CSV_SEPARATOR);
 
                 String content = news.getContent() != null ? news.getContent().replace("\"", "") : "";
-                fr.append("\"" + content + "\"");
-                fr.append(CSV_SEPARATOR);
+                builder.append("\"" + content + "\"");
+                builder.append(CSV_SEPARATOR);
 
-                fr.append("\"" + news.getUrl() + "\"");
-                fr.append(CSV_SEPARATOR);
+                builder.append("\"" + news.getUrl() + "\"");
+                builder.append(CSV_SEPARATOR);
 
-                fr.append("\"" + news.getTime() + "\"");
-                fr.append(System.lineSeparator());
+                builder.append("\"" + news.getTime() + "\"");
+                builder.append(System.lineSeparator());
             }
 
+            fr.write(builder.toString());
             fr.flush();
             fr.close();
         } catch (UnsupportedEncodingException e) {
