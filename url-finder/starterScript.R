7777218cf976f5a9baa1818046292fda986bfc4e
Jeroentjeh <jvandenhoven@mobiquityinc.com>
2017-06-24 14:53:15 +0200

    Updated starter script, now callable from command line. Requires R to be installed, just like: stringr, XML, Rcurl Called like: Rscript <link to script> <link to file> File needs to be a series of titles, each on it's own line. The output will be put in your current folder, according to <title>_output format. Each line will contain a possibly relevant URL

---
+++ url-finder/starterScript.R
@@ -0,0 +1,34 @@
+library(XML)
+library(RCurl)
+library(stringr)
+
+args <- commandArgs()
+args <- args[!str_detect(string = args, pattern = "^-|^/Library")]
+
+stopifnot(length(args) == 1, file.exists(args))
+titles <- readLines(con = args)
+
+getGoogleLinks <- function(google.url) {
+    doc <- getURL(google.url, httpheader = c("User-Agent" = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36"))
+    Sys.sleep(10+rnorm(1,0,2))
+    html <- htmlTreeParse(doc, useInternalNodes = TRUE, error=function
+                          (...){})
+    nodes <- getNodeSet(html, "//h3[@class='r']//a")
+    return(sapply(nodes, function(x) x <- xmlAttrs(x)[["href"]]))
+}
+
+for(title in titles){
+    headline <- URLencode(title)
+    base <- "https://www.google.nl/search?q="
+    url <- paste0(base, headline)
+
+    links <- list()
+    for ( i in  0 : 4 ) {
+        results <- getGoogleLinks(paste0(url, "&start=",i*10))
+        links <- c(links, results)
+    }
+
+    links <- unlist(links)
+    title <- str_replace_all(string = title, pattern = " |/", replacement = "_")
+    write(links, file = paste0(title, "_output"))
+}
