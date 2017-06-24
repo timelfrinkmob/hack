ee62a4edc81b7c8a5ce3fac5f662cd101c033ff0
Jeroentjeh <jvandenhoven@mobiquityinc.com>
2017-06-24 16:03:31 +0200

    Now deposits new file in same folder as input file

--- url-finder/starterScript.R
+++ url-finder/starterScript.R
@@ -5,8 +5,11 @@ library(stringr)
 args <- commandArgs()
 args <- args[!str_detect(string = args, pattern = "^-|^/Library")]
 
+args <- "~/Documents/Mobiquity/Rest/FakeNews/url-finder/test_input"
+
 stopifnot(length(args) == 1, file.exists(args))
 titles <- readLines(con = args)
+folder <- str_replace(string = args, pattern = "/[^/]+$",replacement = "/")
 
 getGoogleLinks <- function(google.url) {
     doc <- getURL(google.url, httpheader = c("User-Agent" = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36"))
@@ -30,5 +33,5 @@ for(title in titles){
 
     links <- unlist(links)
     title <- str_replace_all(string = title, pattern = " |/", replacement = "_")
-    write(links, file = paste0(title, "_output"))
+    write(links, file = paste0(folder, title, "_output"))
 }
