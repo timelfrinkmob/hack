27fc5ce41112c16d2ac1c6aa8f6718fffe641bbb
Mustafa Dagher <dagher.mustafa@gmail.com>
2017-06-25 10:26:28 +0200

    change csv encoding to utf-8

--- news-crowler/src/main/java/ai/mobiquity/sourcer/util/CsvUtils.java
+++ news-crowler/src/main/java/ai/mobiquity/sourcer/util/CsvUtils.java
@@ -3,6 +3,10 @@ package ai.mobiquity.sourcer.util;
 import cn.edu.hfut.dmic.contentextractor.News;
 
 import java.io.*;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.List;
 
 /**
@@ -14,30 +18,28 @@ public class CsvUtils {
 
     public static void writeToCSV(String filename, List<News> newsList) {
         try {
-            FileWriter fr = new FileWriter(filename + ".csv");
+            Path path = Paths.get(filename + ".csv");
+            try (BufferedWriter writer = Files.newBufferedWriter(path, Charset.forName("UTF-8"))) {
+                StringBuilder builder = new StringBuilder();
+                builder.append("Title").append(CSV_SEPARATOR).append("Content").append(CSV_SEPARATOR).append("URL").append(CSV_SEPARATOR).append("Date").append(System.lineSeparator());
+                for (News news : newsList) {
+                    String title = news.getTitle() != null ? news.getTitle().replace("\"", "") : "";
+                    builder.append("\"" + title + "\"");
+                    builder.append(CSV_SEPARATOR);
 
-            StringBuilder builder = new StringBuilder();
-            builder.append("Title").append(CSV_SEPARATOR).append("Content").append(CSV_SEPARATOR).append("URL").append(CSV_SEPARATOR).append("Date").append(System.lineSeparator());
+                    String content = news.getContent() != null ? news.getContent().replace("\"", "") : "";
+                    builder.append("\"" + content + "\"");
+                    builder.append(CSV_SEPARATOR);
 
-            for (News news : newsList) {
-                String title = news.getTitle() != null ? news.getTitle().replace("\"", "") : "";
-                builder.append("\"" + title + "\"");
-                builder.append(CSV_SEPARATOR);
+                    builder.append("\"" + news.getUrl() + "\"");
+                    builder.append(CSV_SEPARATOR);
 
-                String content = news.getContent() != null ? news.getContent().replace("\"", "") : "";
-                builder.append("\"" + content + "\"");
-                builder.append(CSV_SEPARATOR);
+                    builder.append("\"" + news.getTime() + "\"");
+                    builder.append(System.lineSeparator());
+                }
 
-                builder.append("\"" + news.getUrl() + "\"");
-                builder.append(CSV_SEPARATOR);
-
-                builder.append("\"" + news.getTime() + "\"");
-                builder.append(System.lineSeparator());
+                writer.write(builder.toString());
             }
-
-            fr.write(builder.toString());
-            fr.flush();
-            fr.close();
         } catch (UnsupportedEncodingException e) {
         } catch (FileNotFoundException e) {
         } catch (IOException e) {
